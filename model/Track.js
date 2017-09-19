0d230fae6d0d58fc95652d4ab2e5d9ff056857be
Tyler Graf <tylergraf@gmail.com>
2017-09-19 08:28:57 -0600

    Error handling for delete track. Add date to track.

--- model/Track.js
+++ model/Track.js
@@ -5,6 +5,7 @@ const ObjectId = Schema.ObjectId;
 const trackSchema = new Schema({
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now },
   _tracker: {type: ObjectId, ref: 'Tracker'},
   _user: {type: ObjectId, ref: 'User'},
   value: Number
